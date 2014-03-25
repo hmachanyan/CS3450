a0eae6bc7f8026c3fb5d8e88f0999171be1f7ec7
Reese <reese@Reese-linux.(none)>
2014-03-25 08:02:43 -0600

    updating init.php

--- public_html/includes/init.php
+++ public_html/includes/init.php
@@ -2,12 +2,13 @@
 error_reporting(E_ALL);
   ini_set("display_errors", 1);
 	require_once 'config.php';
-	$classDir = 'classes/';
-
-	$files = scandir($classDir);
+	$parent_dir = dirname(dirname($_SERVER['SCRIPT_NAME'])) . '/';
+	echo $parent_dir;
+	$parent_dir = $parent_dir . "includes/classes/";
+	$files = scandir($parent_dir);
 
 	foreach($files as $file){
 		if(strpos($file, '.class.php') !== false && strpos($file, '.swp') === false){
-			require_once $classDir . $file;
+			require_once $parent_dir . $file;
 		}
 	}
